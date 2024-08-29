923c16cc561b381f7923844371376b5a87e0fc10
Heloisa Albuquerque <heloisaalbuquerque1706@gmail.com>
2024-08-29 11:48:01 -0300

    b

--- beecrownd/letras.py
+++ beecrownd/letras.py
@@ -1,9 +1,15 @@
-letra = input()
-frase = input()
+letra = input().strip()
+frase = input().strip()
 palavras = frase.split()
 
-for palavra in palavras:
-    if letra in palavra:
-        print('ok')
-    else:
-        print('fail')
+num_palavras = len(palavras)
+
+contador = sum(letra in palavra for palavra in palavras)
+
+#quant de palavras com a letra * 100 / tot de palavras
+if num_palavras > 0:
+    porcentagem = (contador * 100) / num_palavras
+else:
+    porcentagem = 0.0
+
+print(f'{porcentagem:.1f}')
